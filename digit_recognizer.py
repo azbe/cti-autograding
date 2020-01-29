f1aafb581f5ec4b066e5fc3dff9b4845c77e3d5e
Radu Tudor <tudorpetreradu@gmail.com>
2020-01-29 19:48:08 +0200

    >

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 
 import keras
 import numpy as np
@@ -14,7 +15,7 @@ def build_model():
     net = keras.layers.Flatten()(net)
     net = keras.layers.Dense(128, activation="relu")(net)
     net = keras.layers.Dropout(0.5)(net)
-    net = keras.layers.Dense(10, activation="softmax")(net)
+    net = keras.layers.Dense(4, activation="softmax")(net)
 
     model = keras.models.Model(inputs=inp, outputs=net)
     model.compile(
@@ -25,27 +26,78 @@ def build_model():
     return model
 
 
+def build_discriminator():
+    inp = keras.layers.Input((28, 28, 1))
+    net = inp
+    net = keras.layers.Conv2D(32, (3, 3), activation="relu")(net)
+    net = keras.layers.Conv2D(64, (3, 3), activation="relu")(net)
+    net = keras.layers.MaxPooling2D(pool_size=(2, 2))(net)
+    net = keras.layers.Dropout(0.25)(net)
+    net = keras.layers.Flatten()(net)
+    net = keras.layers.Dense(128, activation="relu")(net)
+    net = keras.layers.Dropout(0.5)(net)
+    net = keras.layers.Dense(1, activation="sigmoid")(net)
+
+    model = keras.models.Model(inputs=inp, outputs=net)
+    model.compile(
+        loss=keras.losses.binary_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+        metrics=["binary_accuracy"])
+
+    return model
+
+
 def load_model(path):
     return keras.models.load_model(path)
 
 
-def train_model(model, epochs, save_path):
+def train_model(dataset_path, epochs, model_path):
     (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+    X_train = X_train[(y_train >= 1) & (y_train <= 4)]
+    X_test = X_test[(y_test >= 1) & (y_test <= 4)]
+    y_train = y_train[(y_train >= 1) & (y_train <= 4)] - 1
+    y_test = y_test[(y_test >= 1) & (y_test <= 4)] - 1
+
+    X_train = 255 - X_train
+    X_test = 255 - X_test
+    for idx in range(1, 3):
+        x = copy.deepcopy(X_train)
+        x[:, :idx, :] = 0
+        x[:, :, :idx] = 0
+        x[:, -idx:, :] = 0
+        x[:, :, -idx:] = 0
+        X_train = np.concatenate([X_train, x], axis=0)
+        y_train = np.concatenate([y_train, y_train], axis=0)
     X_train = np.expand_dims(X_train / 255.0, axis=3)
     X_test = np.expand_dims(X_test / 255.0, axis=3)
-    y_train = keras.utils.to_categorical(y_train, 10)
-    y_test = keras.utils.to_categorical(y_test, 10)
 
+    y_train_oh = keras.utils.to_categorical(y_train, 4)
+    y_test_oh = keras.utils.to_categorical(y_test, 4)
+
+    model = build_model()
     model.fit(
-        X_train, y_train, batch_size=32, epochs=epochs, verbose=1, validation_data=(X_test, y_test),
-        callbacks=[keras.callbacks.ModelCheckpoint(save_path, save_best_only=True)])
+        X_train, y_train_oh, batch_size=32, epochs=10, verbose=1, validation_data=(X_test, y_test_oh), shuffle=True,
+        callbacks=[keras.callbacks.ModelCheckpoint(model_path, save_best_only=True)])
+
+    data_preproc = keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=30,
+        width_shift_range=0.2, 
+        height_shift_range=0.2,
+        zoom_range=0.2,
+        brightness_range=(0.75, 1.25),
+        rescale=1/255.0,
+        validation_split=0.2)
+    train_gen = data_preproc.flow_from_directory(dataset_path, shuffle=True, batch_size=32, target_size=(28, 28), color_mode="grayscale", subset="training")
+    val_gen = data_preproc.flow_from_directory(dataset_path, shuffle=True, batch_size=32, target_size=(28, 28), color_mode="grayscale", subset="validation")
+    model.fit_generator(train_gen, epochs=epochs, callbacks=[keras.callbacks.ModelCheckpoint(model_path, save_best_only=True)],
+                        validation_data=val_gen, validation_steps=10)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--save_path", type=str, default="./model.h5", help="Path to save model to.")
+    parser.add_argument("--dataset_path", type=str, default="./digits", help="Path to digits dataset")
+    parser.add_argument("--model_path", type=str, default="./digit_recognizer.h5", help="Path to save model to.")
     parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to train for")
     args, _ = parser.parse_known_args()
     
-    model = build_model()
-    train_model(model, args.epochs, args.save_path)
+    train_model(args.dataset_path, args.epochs, args.model_path)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import copy
 import os
 import pdb
 import random
+import re
+import time
+import traceback
 
 import cv2
 import numpy as np
@@ -21,12 +24,15 @@ ANSWERS_START_LEFT = (854, 983)
 ANSWERS_START_RIGHT = (866, 3168)
 ANSWERS_INCREASE = (114.5, 140)
 ANSWERS_CELL_SIZE = (60, 80)
-ANSWERS_NO_START_TOP = (265, 3595)
-ANSWERS_NO_START_BOT = (455, 3600)
-ANSWERS_NO_CELL_SIZE = (160, 160)
-ANSWERS_NO_MASK_THRESH = 200
+ANSWERS_NO_START_TOP = (255, 3595)
+ANSWERS_NO_START_BOT = (445, 3600)
+ANSWERS_NO_CELL_SIZE = (200, 200)
+ANSWERS_NO_MASK_THRESH = 60
 ANSWERS_NO_MASK_THRESH_1 = 0.5
 ANSWERS_CHOICES = ("A", "B", "C", "D")
+
+BAREM_INFORMATICA_STR = "Informatica_varianta{}.txt"
+BAREM_FIZICA_STR = "Fizica_varianta{}.txt"
 
 NUM_ANSWERS = 15
 NUM_VARIANTS = 2
@@ -47,6 +53,12 @@ class Answers:
         self._answers = [None for _ in range(NUM_VARIANTS * NUM_ANSWERS)]
         self.subject = Subject.UNKNOWN
         self.subject_nr = None
+
+    def compute(self, barem):
+        correct = 0
+        for idx in range(NUM_VARIANTS * NUM_ANSWERS):
+            correct += self[idx] == barem[idx]
+        return correct
     
     def __getitem__(self, idx):
         return self._answers[idx]
@@ -58,6 +70,11 @@ class Answers:
         return "{} nr. {}\n{}".format(
             str(self.subject), self.subject_nr,
             "\n".join(["{}: {}".format(idx + 1, answer) for idx, answer in enumerate(self._answers)]))
+    
+    def __eq__(self, other):
+        return (self.subject == other.subject) and \
+               (self.subject_nr == other.subject_nr) and \
+               (self._answers == other._answers)
 
 
 def load_image_cv2(path, greyscale=False, noiseless=False, resize=None):
@@ -71,6 +88,16 @@ def load_image_cv2(path, greyscale=False, noiseless=False, resize=None):
     return image
 
 
+def load_answer_file(path):
+    lines = [line.strip().split() for line in open(path, "r").readlines()]
+    answers = Answers()
+    answers.subject = Subject.INFORMATICA if lines[0][0] == "I" else Subject.FIZICA
+    answers.subject_nr = int(lines[0][1])
+    for idx, ans in lines[1:-1]:
+        answers[int(idx)-1] = ans
+    return answers
+
+
 def random_string(length=32):
     return "".join([str(random.randint(0, 9)) for _ in range(length)])
 
@@ -79,19 +106,23 @@ def remove_noise(image, size=3):
     return scipy.ndimage.median_filter(image, size=size)
 
 
-def get_border(answer):
+def get_border(answer, verbose):
     def border_1d(arr):
         idx = 0
-        while arr[idx]: idx += 1
-        while not arr[idx]: idx += 1
+        while idx < arr.size and arr[idx]: idx += 1
+        while idx < arr.size and not arr[idx]: idx += 1
         start = idx
-        while arr[idx]: idx += 1
+        while idx < arr.size and arr[idx]: idx += 1
         end = idx
         return (start, end)
     mask = answer <= ANSWERS_NO_MASK_THRESH
+    if verbose: plt.imshow(answer, cmap="Greys_r"); plt.imshow(mask, alpha=0.3); plt.show()
     mask_h, mask_v = np.mean(mask, axis=0), np.mean(mask, axis=1)
     mask_h, mask_v = (mask_h <= ANSWERS_NO_MASK_THRESH_1), (mask_v <= ANSWERS_NO_MASK_THRESH_1)
-    return border_1d(mask_v), border_1d(mask_h)
+    (sy, ey), (sx, ex) = border_1d(mask_v), border_1d(mask_h)
+    if (ex - sx) == 0 or (ey - sy) / (ex - sx) < 0.5 or (ey - sy) / (ex - sx) > 1.5:
+        raise ValueError("Crop aspect ratio too small or too big.")
+    return (sy, ey), (sx, ex)
 
 
 def normalize(image, template, sift_features=SIFT_FEATURES):
@@ -147,7 +178,7 @@ def crop_answers(image):
     return image
 
 
-def get_answers(image, model):
+def get_answers(image, model, verbose):
     answers = Answers()
 
     iy, ix = ANSWERS_INCREASE
@@ -161,37 +192,71 @@ def get_answers(image, model):
 
     dy, dx = ANSWERS_NO_CELL_SIZE
     crops = [image[sy-dy//2:sy+dy//2, sx-dx//2:sx+dx//2] for (sy, sx) in [ANSWERS_NO_START_TOP, ANSWERS_NO_START_BOT]]
-    borders = [get_border(crop) for crop in crops]
+    borders = [get_border(crop, verbose) for crop in crops]
     crops = [crop[sy:ey, sx:ex] for crop, ((sy, ey), (sx, ex)) in zip(crops, borders)]
     sums = [np.sum(crop) for crop in crops]
     best = np.argmin(sums)
     answers.subject = Subject.INFORMATICA if best == 0 else Subject.FIZICA
 
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(crops[0], cmap="Greys_r")
-    ax[1].imshow(crops[1], cmap="Greys_r")
-    fig.suptitle(answers.subject)
-    plt.show()
+    best_crop = crops[best]
+    best_crop = cv2.resize(best_crop, (28, 28))
+    best_crop = best_crop / 255.0
+    best_crop = np.reshape(best_crop, (1, 28, 28, 1))
+    preds = model.predict(best_crop)
+    answers.subject_nr = np.argmax(preds) + 1
+
+    if verbose: plt.imshow(crops[best], cmap="Greys_r"); plt.show()
+    # cv2.imwrite(os.path.join("output", str(answers.subject_nr - 1), random_string() + ".png"), crops[best])
 
     return answers
 
 
-def main(images, template, model_path):
+def get_result(answers, barem):
+    path = os.path.join(barem, (BAREM_INFORMATICA_STR if answers.subject == Subject.INFORMATICA else BAREM_FIZICA_STR).format(answers.subject_nr))
+    correct_answers = load_answer_file(path)
+    result = answers.compute(correct_answers)
+    return result
+
+
+def compare_answers(answers, groundtruth):
+    gt = load_answer_file(groundtruth)
+    return answers == gt
+
+
+def main(images, barem, groundtruth, template, model_path, verbose):
     template = load_image_cv2(template, greyscale=True, noiseless=True)
     model = digit_recognizer.load_model(model_path)
     images = sorted(images)
-    for idx, path in enumerate(tqdm.tqdm(images)):
-        image = load_image_cv2(path, greyscale=True, noiseless=True)
-        image = normalize(image, template)
-        image = crop_answers(image)
-        answers = get_answers(image, model)
-        visualize_answers(image, answers)
+    for idx, path in enumerate(images):
+        try:
+            t0 = time.time()
+            image = load_image_cv2(path, greyscale=True, noiseless=True)
+            image = normalize(image, template)
+            image = crop_answers(image)
+            answers = get_answers(image, model, verbose)
+            result = get_result(answers, barem)
+            t1 = time.time()
+            result_correct = ""
+            if groundtruth:
+                result_correct = "\033[92mCORRECT\033[0m" if compare_answers(
+                    answers, os.path.join(groundtruth, 
+                                         "image_{}.txt".format(re.findall(r"\d+", os.path.basename(path))[0]))) else \
+                                 "\033[91mWRONG\033[0m"
+            print("[{}/{}] path: \"{}\"\tscore: {} ({:.2f}/10)\ttime: {:.2f}s\t{}".format(
+                idx+1, len(images), path, result, result * 3 / 10 + 1.0, t1 - t0, result_correct))
+            if verbose: visualize_answers(image, answers)
+        except Exception as ex:
+            print("[{}/{}] Error for file \"{}\"\n".format(idx+1, len(images), path), traceback.format_exc())
 
 
 if __name__ == "__main__":
+    np.set_printoptions(suppress=True)
     parser = argparse.ArgumentParser()
     parser.add_argument("images", type=str, nargs="+", help="Paths to input images to grade.")
+    parser.add_argument("--barem", type=str, default="./data/barem", help="Path to directory of files containing the correct answers.")
+    parser.add_argument("--groundtruth", type=str, help="Path to directory containing groundtruth files.")
     parser.add_argument("--template", type=str, default="./template.jpg", help="Path to image templates to use for normalizing perspective.")
-    parser.add_argument("--model_path", type=str, default="./digit_recognizer.h5", help="Path to traineds digit recognizer")
+    parser.add_argument("--model_path", type=str, default="./digit_recognizer.h5", help="Path to trained digit recognizer")
+    parser.add_argument("--verbose", action="store_true")
     args, _ = parser.parse_known_args()
     main(**vars(args))
